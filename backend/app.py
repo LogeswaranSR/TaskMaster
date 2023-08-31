7733c9522b6e9e01ede4bfa5022b1983e258c3ca
LogeswaranSR <logeswaran.sr@hotmail.com>
2023-08-31 15:11:03 +0530

    app.py created
    backend app.py created using Flask API

---
+++ backend/app.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 31 14:47:44 2023
+
+@author: Loges
+"""
+
+from flask import Flask, render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+if __name__=='__main__':
+    app.run(debug=True, port=4000)
