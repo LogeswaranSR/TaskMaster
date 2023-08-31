6a0e6f56d81a186352e6c41ac01834369de87aae
LogeswaranSR <logeswaran.sr@hotmail.com>
2023-08-31 17:37:26 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -20,7 +20,7 @@ class Todo(db.Model):
     completed=db.Column(db.Integer, default=0)
     date_created=db.Column(db.DateTime, default=datetime.utcnow)
     
-    def __repr(self):
+    def __repr__(self):
         return '<Task %r>' % self.id
 
     
